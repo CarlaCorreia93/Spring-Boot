9839e3ec44b894136d20708aca8c1599f9628e49
Carla Correia <carladesousacorreia@bol.com.br>
2021-09-01 02:33:13 -0300

    Hello Word

---
+++ src/main/java/br/org/generation/helloword/controller/HelloWordController.java
@@ -0,0 +1,18 @@
+package br.org.generation.helloword.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWordController {
+
+@GetMapping
+public String hello() {
+	
+	return "Hello World!";
+	
+	}
+
+}
